17c2bfbde1cc88119bc4df46a6971a60d3041ad0
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-09-27 15:18:25 -0700

    wgengine/router{,/osrouter}: split OS router implementations into subpackage
    So wgengine/router is just the docs + entrypoint + types, and then
    underscore importing wgengine/router/osrouter registers the constructors
    with the wgengine/router package.
    
    Then tsnet can not pull those in.
    
    Updates #17313
    
    Change-Id: If313226f6987d709ea9193c8f16a909326ceefe7
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

---
+++ wgengine/router/osrouter/osrouter.go
@@ -0,0 +1,8 @@
+// Copyright (c) Tailscale Inc & AUTHORS
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package osrouter contains OS-specific router implementations.
+// This package has no API; it exists purely to import
+// for the side effect of it registering itself with the wgengine/router
+// package.
+package osrouter
